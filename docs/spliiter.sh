9479c3df0b754c15ecc70db8280599922c0eaaec
Andrew Levin <andrewlevingit@hotmail.com>
2023-07-31 02:07:50 +0300

    1690758470

--- docs/spliiter.sh
+++ docs/spliiter.sh
@@ -1,11 +1,12 @@
 #all=$(find . -maxdepth 100 -type f -not -path '*/\.*')
 all=$(ls -R)
+NOW=`date`
 
 for entry in $all*
 do
   	echo "$entry"
 	git add "$entry"
-	git commit -m date
+	git commit -m "$date"
 	git push
 done
 
